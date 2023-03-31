73f68b650be59ebe2b03f3875d22c946ead427ac
Sekhgroup <124693293+Sekhgroup@users.noreply.github.com>
2023-03-31 16:05:41 +0530

    add js for hit counter

--- used/js/script.js
+++ used/js/script.js
@@ -198,3 +198,22 @@ const observer = new IntersectionObserver((entries, observer) => {
 loadimages.forEach(img => {
   observer.observe(img);
 });
+
+// idenhi ivdi balvisi likki 10 arba .............................................................................
+
+var clickCount = 0;
+var clickDiv = document.getElementById("clickDiv");
+var hiddenDiv = document.getElementById("hdn");
+
+if (!clickDiv || !hiddenDiv) {
+  console.error("Could not find clickDiv or hiddenDiv");
+} else {
+  clickDiv.addEventListener("click", function() {
+    clickCount++;
+    if (clickCount === 10) {
+      hiddenDiv.style.display = "block";
+    }
+  });
+}
+
+
