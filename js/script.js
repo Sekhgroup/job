384a3e3271795f638840e2330dd29867374d9642
Sekhgroup <124693293+Sekhgroup@users.noreply.github.com>
2023-03-31 19:14:56 +0530

    full updated js

--- js/script.js
+++ js/script.js
@@ -44,141 +44,10 @@ window.addEventListener("scroll", function () {
 /** @@@@@@@@@@@@@ MY CODES START FROM HERE@@@@@@@@@@@@@@@@@@ **/
 
 
-
-
-
-
-
-// SEARCH BAR....................................................................
-
-function searchList() {
-  // Get the search term and convert to lowercase
-  var input = document.getElementById("mySearch");
-  var filter = input.value.toLowerCase();
-
-  // Get the list and list items
-  var ul = document.getElementById("myList");
-  var li = ul.getElementsByTagName("li");
-
-  // Loop through all list items, and hide those that don't match the search query
-  for (var i = 0; i < li.length; i++) {
-    var text = li[i].textContent.toLowerCase();
-    if (text.indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-}
-
-// Add event listener to the search input
-document.getElementById("mySearch").addEventListener("keyup", searchList);
-
-
-
-
-
-
-/*// ONCLICK SERVICE NAME ENTER IN ORDER FORM ...........................................................................
-
-
-// querySelectorAll method to get all the li elements and then use the querySelector method to get the h3 element inside each li element if it exists, otherwise get the text of the li element. Here’s the updated code:*/
-
-// get all the li elements
-const listItems = document.querySelectorAll('li');
-
-// attach a click event listener to each li
-listItems.forEach((li) => {
-  li.addEventListener('click', () => {
-    // get the h3 element inside the clicked li if it exists, otherwise get the text of the li
-    const h3 = li.querySelector('h3');
-    const text = h3 ? h3.innerText : li.innerText;
-    
-    // update the text of the service_type div with the text
-    const output = document.getElementById('service_type');
-    output.value = text;
-    
-    console.log("yes");
-  });
-});
-// This should get the text of the h3 element inside the clicked li element if it exists, otherwise get the text of the li element.
-
-
-
-
-// ONCLICK  ORDER FORM POPUP...............................................................................................
-
-function fn(){
-  let FullBox = document.getElementById("FullBox");
-  let popup = document.getElementById("Order");
-  var Service = document.getElementById("Service");
-
-  if (FullBox.style.display === "flex") {
-    setTimeout(function() {
-      popup.style.display = "block";
-      FullBox.style.display = "none";
-      popup.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" }); //scroll kore popup div e niye jabe
-      Service.style.backgroundColor = "var(--bright-navy-blue)";
-    }, 10); // 2-second delay
-  }
-}
-
-
-// POPUP CLOSE FUNCTION .......................................................................................................
-
-function Closefn(){
-  let FullBox = document.getElementById("FullBox");
-  let popup = document.getElementById("Order");
-
-  if (popup.style.display === "block") {
-    popup.style.display = "none";
-    FullBox.style.display = "flex";
-    Service.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" }); //scroll kore popup div e niye jabe
-    Service.style.backgroundColor = "white";
-}
-}
-
-
-// TYPING PLACE HOLDER ...............................................................................................
-
-var sbar = document.getElementById("mySearch");
-var placeholder = sbar.getAttribute("placeholder");
-var i = 0;
-setInterval(function() {
-  if (i < placeholder.length) {
-    sbar.setAttribute("placeholder", placeholder.slice(0, i) + " |");
-    i++;
-  } else {
-    sbar.setAttribute("placeholder", placeholder.slice(0, i));
-    i = 0;
-  }
-}, 200);
-
-
-// idenhi ivdi balvisi likki 10 arba .............................................................................
-
-var clickCount = 0;
-var clickDiv = document.getElementById("clickDiv");
-var hiddenDiv = document.getElementById("hdn");
-
-if (!clickDiv || !hiddenDiv) {
-  console.error("Could not find clickDiv or hiddenDiv");
-} else {
-  clickDiv.addEventListener("click", function() {
-    clickCount++;
-    if (clickCount === 10) {
-      hiddenDiv.style.display = "block";
-    }
-  });
-}
-
-
-
-
-// loading timing add in carousel.........................................
+// loading timing add in carousel
 
 // Get the slider div and its images
-var sliderDiv = document.getElementById('carousel-cell');
+var sliderDiv = document.getElementById('slider');
 var images = sliderDiv.getElementsByTagName('img');
 
 // Hide the images initially
@@ -221,3 +90,136 @@ window.addEventListener('load', function() {
     });
   }
 });
+
+
+
+
+
+// SEARCH BAR 
+
+function searchList() {
+  // Get the search term and convert to lowercase
+  var input = document.getElementById("mySearch");
+  var filter = input.value.toLowerCase();
+
+  // Get the list and list items
+  var ul = document.getElementById("myList");
+  var li = ul.getElementsByTagName("li");
+
+  // Loop through all list items, and hide those that don't match the search query
+  for (var i = 0; i < li.length; i++) {
+    var text = li[i].textContent.toLowerCase();
+    if (text.indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+// Add event listener to the search input
+document.getElementById("mySearch").addEventListener("keyup", searchList);
+
+
+
+
+
+
+
+
+/*// ONCLICK SERVICE NAME ENTER IN ORDER FORM 
+
+
+// querySelectorAll method to get all the li elements and then use the querySelector method to get the h3 element inside each li element if it exists, otherwise get the text of the li element. Here’s the updated code:*/
+
+// get all the li elements
+const listItems = document.querySelectorAll('li');
+
+// attach a click event listener to each li
+listItems.forEach((li) => {
+  li.addEventListener('click', () => {
+    // get the h3 element inside the clicked li if it exists, otherwise get the text of the li
+    const h3 = li.querySelector('h3');
+    const text = h3 ? h3.innerText : li.innerText;
+    
+    // update the text of the service_type div with the text
+    const output = document.getElementById('service_type');
+    output.value = text;
+    
+    console.log("yes");
+  });
+});
+// This should get the text of the h3 element inside the clicked li element if it exists, otherwise get the text of the li element.
+
+
+
+
+
+// ONCLICK  ORDER FORM POPUP
+
+function fn(){
+  let FullBox = document.getElementById("FullBox");
+  let popup = document.getElementById("Order");
+  var Service = document.getElementById("Service");
+
+  if (FullBox.style.display === "flex") {
+    setTimeout(function() {
+      popup.style.display = "block";
+      FullBox.style.display = "none";
+      popup.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" }); //scroll kore popup div e niye jabe
+      Service.style.backgroundColor = "var(--bright-navy-blue)";
+    }, 10); // 2-second delay
+  }
+}
+
+
+
+
+
+// POPUP CLOSE FUNCTION 
+
+function Closefn(){
+  let FullBox = document.getElementById("FullBox");
+  let popup = document.getElementById("Order");
+
+  if (popup.style.display === "block") {
+    popup.style.display = "none";
+    FullBox.style.display = "flex";
+    Service.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" }); //scroll kore popup div e niye jabe
+    Service.style.backgroundColor = "white";
+}
+}
+
+
+// TYPING PLACE HOLDER 
+
+var sbar = document.getElementById("mySearch");
+var placeholder = sbar.getAttribute("placeholder");
+var i = 0;
+setInterval(function() {
+  if (i < placeholder.length) {
+    sbar.setAttribute("placeholder", placeholder.slice(0, i) + " |");
+    i++;
+  } else {
+    sbar.setAttribute("placeholder", placeholder.slice(0, i));
+    i = 0;
+  }
+}, 200);
+
+
+// idenhi ivdi balvisi likki 10 arba 
+
+var clickCount = 0;
+var clickDiv = document.getElementById("clickDiv");
+var hiddenDiv = document.getElementById("hdn");
+
+if (!clickDiv || !hiddenDiv) {
+  console.error("Could not find clickDiv or hiddenDiv");
+} else {
+  clickDiv.addEventListener("click", function() {
+    clickCount++;
+    if (clickCount === 10) {
+      hiddenDiv.style.display = "block";
+    }
+  });
+}
