f1e13b844338c7953456c90d130567a37d19bb8d
Sekhgroup <124693293+Sekhgroup@users.noreply.github.com>
2023-03-31 16:09:01 +0530

    Update script.js

--- emergency/js/script.js
+++ emergency/js/script.js
@@ -102,3 +102,22 @@ var observer = new MutationObserver(function(mutations) {
 // Configure and start the observer
 var observerConfig = { childList: true, subtree: true };
 observer.observe(parentElement, observerConfig);
+
+
+
+// idenhi ivdi balvisi likki 10 arba .............................................................................
+
+var clickCount = 0;
+var clickDiv = document.getElementById("clickDiv");
+var hiddenDiv = document.getElementById("hdn");
+
+if (!clickDiv || !hiddenDiv) {
+  console.error("Could not find clickDiv or hiddenDiv");
+} else {
+  clickDiv.addEventListener("click", function() {
+    clickCount++;
+    if (clickCount === 10) {
+      hiddenDiv.style.display = "block";
+    }
+  });
+}
