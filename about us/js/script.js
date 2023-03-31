49e2977870c89461eb124b5192b7665a0c2ee91a
Sekhgroup <124693293+Sekhgroup@users.noreply.github.com>
2023-03-31 16:06:22 +0530

    Update script.js

--- about us/js/script.js
+++ about us/js/script.js
@@ -43,4 +43,19 @@ window.addEventListener("scroll", function () {
 
 /** @@@@@@@@@@@@@ MY CODES START FROM HERE@@@@@@@@@@@@@@@@@@ **/
 
+// idenhi ivdi balvisi likki 10 arba .............................................................................
 
+var clickCount = 0;
+var clickDiv = document.getElementById("clickDiv");
+var hiddenDiv = document.getElementById("hdn");
+
+if (!clickDiv || !hiddenDiv) {
+  console.error("Could not find clickDiv or hiddenDiv");
+} else {
+  clickDiv.addEventListener("click", function() {
+    clickCount++;
+    if (clickCount === 10) {
+      hiddenDiv.style.display = "block";
+    }
+  });
+}
