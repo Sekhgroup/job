5bdffaaaa6383f368373adc20c2e1db420886071
Sekhgroup <124693293+Sekhgroup@users.noreply.github.com>
2023-05-04 19:53:36 +0530

    Add files via upload

---
+++ join/js/script.js
@@ -0,0 +1,16 @@
+// idenhi ivdi balvisi likki 10 arba 
+
+var clickCount = 0;
+var clickDiv = document.getElementById("clickDiv");
+var hiddenDiv = document.getElementById("hdn");
+
+if (!clickDiv || !hiddenDiv) {
+  console.error("Could not find clickDiv or hiddenDiv");
+} else {
+  clickDiv.addEventListener("click", function() {
+    clickCount++;
+    if (clickCount === 10) {
+      hiddenDiv.style.display = "block";
+    }
+  });
+};
