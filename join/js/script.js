8012c90b251f755857c2f021e1ad259803ed41d2
Sekhgroup <124693293+Sekhgroup@users.noreply.github.com>
2023-05-23 14:21:53 +0530

    submit update

--- join/js/script.js
+++ join/js/script.js
@@ -14,3 +14,38 @@ if (!clickDiv || !hiddenDiv) {
     }
   });
 };
+
+
+
+
+//***************one time click and text change submit button***************************** */
+
+document.addEventListener("DOMContentLoaded", function() {
+    var button = document.getElementById("formSubmit");
+    button.addEventListener("click", function() {
+      setTimeout(function() {
+        disableButton();
+      }, 200); // 200 milliseconds = .2 second
+    });
+  
+    function disableButton() {
+      button.disabled = true;
+      button.style.background = "#4ff918";
+      var originalText = button.innerHTML;
+      var dots = 0;
+      button.innerHTML = "Submitting";
+  
+      var intervalId = setInterval(function() {
+        dots = (dots + 1) % 4;
+        button.innerHTML = "Submitting" + Array(dots + 1).join(".");
+      }, 500); // 500 milliseconds = 0.5 second
+  
+      // Simulating asynchronous task completion
+      setTimeout(function() {
+        clearInterval(intervalId);
+        button.innerHTML = "Submitted!";
+        // Perform additional submission logic here
+      }, 3000); // Simulating 3 seconds of submission time
+    }
+  });
+  
